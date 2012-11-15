bf72b1fa7c18eb2c3de57f83501610dd18e42be5
jvoliveiran@gmail.com <jvoliveiran@gmail.com@bbc39dd4-ed52-91a9-6ee3-6e22d8ac3dee>
2012-11-15 22:35:29 +0000

    Cadastro de Funcionario

--- SCTransporte/app/models/Funcionario.java
+++ SCTransporte/app/models/Funcionario.java
@@ -1,22 +1,51 @@
 package models;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import play.db.jpa.GenericModel;
 
+@Entity
 public class Funcionario extends GenericModel{
 
 	 @Id
 	 @GeneratedValue
-	 public Long id;
+	 private Long id;
+	 
+	 @ManyToOne
+	 @JoinColumn (name = "id_usuario")
+	 private Usuario usuario;
+	 
+	 @ManyToOne
+	 @JoinColumn (name = "id_cargo")
+	 private Cargo cargo;
 
 	 public Long getId() {
-	    return id;
+		 return id;
 	 }
 
-	 @Override
-	 public Object _key() {
-	    return getId();
+	 public void setId(Long id) {
+		 this.id = id;
 	 }
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Cargo getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+	 
+	 
 }
