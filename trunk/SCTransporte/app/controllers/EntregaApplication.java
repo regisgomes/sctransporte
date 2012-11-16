685e3890c9cc1858af7e665095365a1a405e4ff8
jvoliveiran@gmail.com <jvoliveiran@gmail.com@bbc39dd4-ed52-91a9-6ee3-6e22d8ac3dee>
2012-11-16 20:25:31 +0000

    Cadastro de Entrega

---
+++ trunk/SCTransporte/app/controllers/EntregaApplication.java
@@ -0,0 +1,59 @@
+package controllers;
+
+import java.util.Date;
+import java.util.List;
+
+import models.Cliente;
+import models.Entrega;
+import models.Funcionario;
+import play.mvc.Controller;
+
+public class EntregaApplication extends Controller{
+	
+	public static void cadastroEntrega(){
+		List<Cliente> clientes = Cliente.all().fetch();
+		List<Funcionario> funcionarios = Funcionario.all().fetch();
+		render(clientes, funcionarios);
+	}
+	
+	public static void cadastroEntrega(String msgErro){
+		List<Cliente> clientes = Cliente.all().fetch();
+		List<Funcionario> funcionarios = Funcionario.all().fetch();
+		render(clientes, funcionarios, msgErro);
+	}
+
+	public static void cadastrarEntrega(Long idClienteDestino, Long idClienteOrigem,
+			Long idFuncionario, String codRastreio, String descricao, Integer volumes, Double valor){
+		
+		if(idClienteDestino != null && idClienteOrigem != null && idFuncionario != null && codRastreio != null 
+				&& !codRastreio.isEmpty() && descricao != null && !descricao.isEmpty() && volumes != null && valor != null){
+			
+			Entrega entrega = new Entrega(codRastreio, descricao, volumes, valor);
+			
+			Cliente origem = Cliente.findById(idClienteOrigem);
+			Cliente destino = Cliente.findById(idClienteDestino);
+			
+			Funcionario func = Funcionario.findById(idFuncionario);
+			
+			entrega.setIdClienteDestino(destino);
+			entrega.setIdClienteOrigem(origem);
+			entrega.setIdFuncionario(func);
+			
+			entrega.setCodRastreio(codRastreio);
+			entrega.setDescricao(descricao);
+			entrega.setVolumes(volumes);
+			entrega.setValor(valor);
+			
+			entrega.setData(new Date());
+		
+			entrega.save();
+		}
+		else{
+			String msgErro = "Informe todos os dados do formulario";
+			cadastroEntrega(msgErro);
+		}
+		String msgInformation = "Entrega Cadastrada com sucesso!";
+		Application.menu(Application.getUsuarioLogado(), msgInformation);
+		
+	}
+}
