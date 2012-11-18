784d1a88ccf3e7f404c6a43b10103e5bd70f62fd
jvoliveiran@gmail.com <jvoliveiran@gmail.com@bbc39dd4-ed52-91a9-6ee3-6e22d8ac3dee>
2012-11-18 22:40:25 +0000

    Ediçao marca

--- trunk/SCTransporte/app/models/Marca.java
+++ trunk/SCTransporte/app/models/Marca.java
@@ -4,22 +4,41 @@
 package models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-import play.db.jpa.Model;
+import play.db.jpa.GenericModel;
 
 /**
  * @author rafaelholanda90
  *
  */
 @Entity
-public class Marca extends Model {
+public class Marca extends GenericModel {
 
 	private String nome;
+	
+	@Id
+    @GeneratedValue
+    public Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public Object _key() {
+        return getId();
+    }
 
 	public Marca(String nome) {
 		this.nome = nome;
 	}
 	
+	public Marca() {
+		// TODO Auto-generated constructor stub
+	}
+
 	public String getNome() {
 		return nome;
 	}
