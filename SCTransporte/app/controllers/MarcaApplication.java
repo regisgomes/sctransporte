7f1de585b185d1f76f443fb0e828f76d2d6449c9
jvoliveiran@gmail.com <jvoliveiran@gmail.com@bbc39dd4-ed52-91a9-6ee3-6e22d8ac3dee>
2012-11-18 22:40:25 +0000

    Ediçao marca

--- SCTransporte/app/controllers/MarcaApplication.java
+++ SCTransporte/app/controllers/MarcaApplication.java
@@ -18,22 +18,34 @@ public class MarcaApplication extends Controller {
 		List<Marca> marcas = Marca.all().fetch();
 		render(marcas);
 	}
+	
+	public static void alterarMarca(String idMarca){
+		Long id = Long.parseLong(idMarca);
+		Marca marca = Marca.findById(id);
+		cadastroMarca(marca, null);
+	}
 
-	public static void cadastroMarca() {
-		render();
+	public static void cadastroMarca(Marca marca, String msgErro) {
+		if(marca == null)
+			marca = new Marca();
+		
+		render(marca, msgErro);
 	}
 	
-	public static void cadastroMarca(String msgErro) {
-		render(msgErro);
-	}
-	
-	public static void cadastrarMarca(String nomeMarca) {
-		if (nomeMarca != null && !nomeMarca.isEmpty()) {
+	public static void cadastrarMarca(String nomeMarca, String idMarca) {
+		if(idMarca != null && !idMarca.isEmpty()){
+			Long id = Long.parseLong(idMarca);
+			System.out.println(idMarca + " : " + id);
+			Marca marca = Marca.findById(id);
+			marca.setNome(nomeMarca);
+			marca.save();
+		}
+		else if (nomeMarca != null && !nomeMarca.isEmpty()) {
 			Marca marca = new Marca(nomeMarca);
 			marca.save();
 		} else {
 			String msgErro = "Informe o nome para a Marca!";
-			cadastroMarca(msgErro);
+			cadastroMarca(null, msgErro);
 		}
 		
 		String msgInformation = "Marca "+ nomeMarca +" Cadastrada com sucesso!";
