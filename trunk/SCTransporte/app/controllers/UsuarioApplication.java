8a8a779f8b70220ff7495788719bec33398573d5
jvoliveiran@gmail.com <jvoliveiran@gmail.com@bbc39dd4-ed52-91a9-6ee3-6e22d8ac3dee>
2012-11-30 02:30:55 +0000

    Edição usuario

--- trunk/SCTransporte/app/controllers/UsuarioApplication.java
+++ trunk/SCTransporte/app/controllers/UsuarioApplication.java
@@ -13,26 +13,52 @@ public class UsuarioApplication extends Controller{
 		render(users);
 	}
 	
-	public static void cadastroUsuario(){
-		render();
+	public static void alterarUsuario(String idUsuario){
+		Long id = Long.parseLong(idUsuario);
+		Usuario usuario = Usuario.findById(id);
+		cadastroUsuario(usuario, null);
 	}
 	
-	public static void cadastroUsuario(String msgErro){
-		render(msgErro);
+	public static void cadastroUsuario(Usuario usuario, String msgErro){
+		if(usuario == null){
+			usuario = new Usuario();
+		}
+		render(usuario, msgErro);
 	}
 	
-	public static void cadastrarUsuario(String login, String pass, String nome, String cpf,
+	public static void cadastrarUsuario(String idUsuario, String login, String pass, String nome, String cpf,
 			String endereco, String telefone){
-		if(login != null && !login.isEmpty() && pass != null && !pass.isEmpty()){
-			Usuario user = new Usuario(login, pass, nome, cpf, endereco, telefone);
-			user.save();
+		
+		Usuario usuario = new Usuario();
+		//Editar
+		if(idUsuario != "" && !idUsuario.isEmpty()){
+			Long id = Long.parseLong(idUsuario);
+			usuario = Usuario.findById(id);
+			
+			usuario.setLogin(login);
+			usuario.setPass(pass);
+			usuario.setNome(nome);
+			usuario.setCpf(cpf);
+			usuario.setEndereco(endereco);
+			usuario.setTelefone(telefone);
+			usuario.save();
+			String msgInformation = "Usuario editado com sucesso";
+			Application.menu(Application.getUsuarioLogado(), msgInformation);
 		}
+		
+		//Inserir
 		else{
-			String msgErro = "Informe um login e uma senha para o usuario";
-			cadastroUsuario(msgErro);
+			if(login != null && !login.isEmpty() && pass != null && !pass.isEmpty()){
+				usuario = new Usuario(login, pass, nome, cpf, endereco, telefone);
+				usuario.save();
+			}
+			else{
+				String msgErro = "Informe um login e uma senha para o usuario";
+				cadastroUsuario(null, msgErro);
+			}
+			String msgInformation = "Usuario cadastrado com sucesso!";
+			Application.menu(Application.getUsuarioLogado(), msgInformation);
 		}
-		String msgInformation = "Usuario cadastrado com sucesso!";
-		Application.menu(Application.getUsuarioLogado(), msgInformation);
 	}
 
 }
